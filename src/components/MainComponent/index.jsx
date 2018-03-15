f9781db4e07ce3a9df9b2e8a9488e3e58feb05f4
Angel Mendez <angelmendezz751@gmail.com>
2018-03-15 11:28:02 -0600

    Install need module

--- src/components/MainComponent/index.jsx
+++ src/components/MainComponent/index.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
-export default class MainComponent extends Component{
+import ReactTouchEvents from "react-touch-events";
+export default class MainComponent extends Component {
 
-    render(){
-        return(
-            <div data-element='main_component' className='main_component' >
-                Tocuh this section
-            </div>
+    handleTap () {
+    
+        console.log("you have taped me");
+    
+    }
+    
+    handleSwipe (direction) {
+    
+        switch (direction) {
+            case "top":
+            case "bottom":
+            case "left":
+            case "right":
+            
+                console.log(`you swiped ${direction}`)
+        
+        }
+    }
+    render() {
+        return (
+            <ReactTouchEvents
+                onTap={this.handleTap.bind(this)}
+                onSwipe={this.handleSwipe.bind(this)}
+            >
+                <div data-element='main_component' className='main_component' >
+                    Tocuh this section
+                </div>
+            </ReactTouchEvents>
         )
     }
 
