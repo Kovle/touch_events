ab3ae097c364a13bb4cc201557c5b733e62d8dab
Angel Mendez <angelmendezz751@gmail.com>
2018-03-15 11:24:49 -0600

    Create MainComponent

---
+++ src/components/MainComponent/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export default class MainComponent extends Component{
+
+    render(){
+        return(
+            <div data-element='main_component' className='main_component' >
+                Tocuh this section
+            </div>
+        )
+    }
+
+}
